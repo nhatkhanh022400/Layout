003f7f8e3eaf0a553d7fa99b67468c83938ba1f2
nhatkhanh0204 <nhatkhanh0204@gmail.com>
2022-04-06 14:06:27 +0700

    chỉnh layout - UI/UX - Update: Chỉnh Lại FE của Admin và User

---
+++ Page-User/.history/js/user_20220406114643.js
@@ -0,0 +1,59 @@
+var app = angular.module('user-app', []);
+app.controller('abc', function abc($scope) {
+    $scope.insides = [
+        {
+            img: 'images/blog-1.jpg',
+            dayStart: '24/03/2022',
+            teach: 'Bởi: Giảng Viên',
+            title: 'Tên Khóa Học',
+            
+        },
+        {
+            img: 'images/blog-2.jpg',
+            dayStart: '24/03/2022',
+            teach: 'Bởi: Giảng Viên',
+            title: 'Tên Khóa Học',
+            
+        },
+        {
+            img: 'images/blog-3.jpg',
+            dayStart: '24/03/2022',
+            teach: 'Bởi: Giảng Viên',
+            title: 'Tên Khóa Học',
+            
+        },
+    ];
+
+});
+
+
+app.controller('abcd', function abcd($scope) {
+    $scope.outsides = [
+        {
+            img: 'images/blog-1.jpg',
+            dayStart: '24/03/2022',
+            teach: 'Bởi: Giảng Viên',
+            title: 'Tên Khóa Học',
+            issued: 'Cycber Soft'
+        },
+        {
+            img: 'images/blog-2.jpg',
+            dayStart: '24/03/2022',
+            teach: 'Bởi: Giảng Viên',
+            title: 'Tên Khóa Học',
+            issued: 'Cycber Soft'
+        },
+        {
+            img: 'images/blog-3.jpg',
+            dayStart: '24/03/2022',
+            teach: 'Bởi: Giảng Viên',
+            title: 'Tên Khóa Học',
+            issued: 'Cycber Soft'
+        },
+    ];
+});
+
+
+
+
+
---
+++ Page-User/.history/js/user_20220406121652.js
@@ -0,0 +1,60 @@
+var app = angular.module('user-app', []);
+app.controller('abc', function abc($scope) {
+    $scope.insides = [
+        {
+            img: 'images/blog-1.jpg',
+            dayStart: '24/03/2022',
+            teach: 'Bởi: Giảng Viên',
+            title: 'Tên Khóa Học',
+            
+        },
+        {
+            img: 'images/blog-2.jpg',
+            dayStart: '24/03/2022',
+            teach: 'Bởi: Giảng Viên',
+            title: 'Tên Khóa Học',
+            
+        },
+        {
+            img: 'images/blog-3.jpg',
+            dayStart: '24/03/2022',
+            teach: 'Bởi: Giảng Viên',
+            title: 'Tên Khóa Học',
+            
+        },
+    ];
+
+});
+
+
+app.controller('abcd', function abcd($scope) {
+    $scope.test = {};
+    $scope.outsides = [
+        {
+            img: 'images/blog-1.jpg',
+            time: '6 tháng',
+            teach: 'Bởi: Giảng Viên',
+            title: 'Tên Khóa Học',
+            issued: 'Cycber Soft'
+        },
+        {
+            img: 'images/blog-2.jpg',
+            time: '6 tháng',
+            teach: 'Bởi: Giảng Viên',
+            title: 'Tên Khóa Học',
+            issued: 'Cycber Soft'
+        },
+        {
+            img: 'images/blog-3.jpg',
+            time: '6 tháng',
+            teach: 'Bởi: Giảng Viên',
+            title: 'Tên Khóa Học',
+            issued: 'Cycber Soft'
+        },
+    ];
+});
+
+
+
+
+
