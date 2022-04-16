9dfb2072d8c45560e4b5534d8fa4b2467eddfc60
nhatkhanh0204 <nhatkhanh0204@gmail.com>
2022-04-16 14:32:19 +0700

    admin, user - UI/UX - update: chỉnh sửa và bổ sung thêm

--- Page-Admin/js/teacher.js
+++ Page-Admin/js/teacher.js
@@ -1,44 +1,49 @@
 var app = angular.module("myapp", []);
-app.controller("viewCril", function (Excel, $timeout, $scope) {
-    $scope.test = {};
-    $scope.index = -1;
 
-    // add
-    $scope.save = function () {
-        $scope.listCourses.push(angular.copy($scope.test))
-    };
-    // edit
-    $scope.edit = function (index) {
-        $scope.index = index;
-        $scope.test = angular.copy($scope.listCourses[index]);
-    }
-    $scope.update = function () {
-        $scope.listCourses[$scope.index] = angular.copy($scope.test)
-    }
-    // delete
-    $scope.delete = function () {
-        $scope.listCourses.splice($scope.index, 1);
-        $scope.cancel();
-    }
-    $scope.cancel = function () {
+function getAPI(urlAPI) {
+    app.controller("viewCril", function (Excel, $timeout, $scope) {
         $scope.test = {};
-    }
-    $scope.listCourses = [
-        {
-            name: "Jayce",
-            phone: "012345678",
-            email: "abc@gmail.com",
-            department: "Khoa CNTT",
+        $scope.index = -1;
+    
+        // add
+        $scope.save = function () {
+            $scope.listCourses.push(angular.copy($scope.test))
+        };
+        // edit
+        $scope.edit = function (index) {
+            $scope.index = index;
+            $scope.test = angular.copy($scope.listCourses[index]);
         }
-    ]
+        $scope.update = function () {
+            $scope.listCourses[$scope.index] = angular.copy($scope.test)
+        }
+        // delete
+        $scope.delete = function () {
+            $scope.listCourses.splice($scope.index, 1);
+            $scope.cancel();
+        }
+        $scope.cancel = function () {
+            $scope.test = {};
+        }
+        $scope.listCourses = [
+            {
+                name: "Jayce",
+                phone: "012345678",
+                email: "abc@gmail.com",
+                department: "Khoa CNTT",
+            }
+        ]
+    
+        //Xuất file Excel
+    
+        $scope.exportToExcel = function (tableId) { // ex: '#my-table'
+            var exportHref = Excel.tableToExcel(tableId, 'WireWorkbenchDataExport');
+            $timeout(function () { location.href = exportHref; }, 100); // trigger download
+        }
+    })
+};
 
-    //Xuất file Excel
 
-    $scope.exportToExcel = function (tableId) { // ex: '#my-table'
-        var exportHref = Excel.tableToExcel(tableId, 'WireWorkbenchDataExport');
-        $timeout(function () { location.href = exportHref; }, 100); // trigger download
-    }
-})
 
 
 app.factory('Excel', function ($window) {
